94fafc801485c558ef8af368a6d37278e2c271ae
bfrasure <bill@solutionreach.com>
2017-11-11 01:22:45 -0700

    +Actor musings  #time 10m

--- src/main/scala/com/billding/kafka/ProducerActor.scala
+++ src/main/scala/com/billding/kafka/ProducerActor.scala
@@ -13,6 +13,14 @@ class RawWeatherActor extends Actor {
   /*
     Maybe the reason that actors have signatures that can see anything, and do anything in response, is that real entities
     are always present and might interact with the world in any number of ways that you can't predict from your vantage point.
+
+
+    Actors are just going to act.
+    No actor knows what  other actors will be available.
+    They just say their lines based on the line delivered to them.
+    This might also include jumping a few steps ahead, if the actor communicating with them flubbed it.
+      - But I don't know that.
+      - Or going back in lines, if it's important enough that you need to re-prompt them.
    */
   def receive: PartialFunction[Any, Unit] = {
     case RawWeatherActor.START_PRODUCING_WEATHER => {
