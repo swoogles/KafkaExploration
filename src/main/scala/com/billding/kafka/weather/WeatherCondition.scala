df77f55bc2e6c535275919a73bfd86d99b345633
bfrasure <bill@solutionreach.com>
2017-11-11 01:11:39 -0700

    +WeatherCondtion class  #time 20m

---
+++ src/main/scala/com/billding/kafka/weather/WeatherCondition.scala
@@ -0,0 +1,22 @@
+package com.billding.kafka.weather
+
+object WeatherCondition {
+  val values = List(Snow, Rain)
+
+  object Snow extends WeatherCondition {
+    val name = "Snow"
+    val idx = 0
+  }
+
+  object Rain extends WeatherCondition {
+    val name = "Rain"
+    val idx = 1
+  }
+
+}
+
+sealed trait WeatherCondition {
+  val name: String
+  val idx: Int
+}
+
