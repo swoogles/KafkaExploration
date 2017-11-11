d9f2d670962a6e88065ddc164e095d4555f8521e
bfrasure <bill@solutionreach.com>
2017-11-11 01:14:26 -0700

    Implement FunAlerter  #time 15m

--- src/main/scala/com/billding/kafka/FunAlerter.scala
+++ src/main/scala/com/billding/kafka/FunAlerter.scala
@@ -1,5 +1,37 @@
+
 package com.billding.kafka
 
-class FunAlerter {
+import java.time.Duration
 
+import com.billding.timing.TimedFunctions
+import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords}
+
+import scala.collection.JavaConverters._
+
+class FunAlerter(timedFunctions: TimedFunctions) {
+  /*
+  Maybe each Actor should receive a "tick" that tells it to poll for a period.
+  I think this could be done in a way that doesn't involve deadlocking processes.
+   */
+  val kafkaProps = new KafkaConfig()
+  val bidirectionalKafka: BidirectionalKafka = new BidirectionalKafka(kafkaProps.RAW_WEATHER, kafkaProps.PLEASURE_TOPIC)
+
+  def run() = {
+    val determinePurchases: () => Unit = () => {
+
+      val records: ConsumerRecords[String, String] = bidirectionalKafka.consumer.poll(100)
+      for (record: ConsumerRecord[String, String] <- records.asScala) {
+        if (record.value.contains("snow")) {
+          bidirectionalKafka.send(
+            "key",
+            s"Snow is coming! Get ready to shred!"
+          )
+        }
+      }
+    }
+
+    timedFunctions.doForPeriodOfTime(
+      Duration.ofSeconds(1), "funAlerter"
+    )(determinePurchases)
+  }
 }
