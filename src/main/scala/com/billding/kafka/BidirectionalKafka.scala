b6967fd8e217fd426e9b55ddac73476f5194ae2d
bfrasure <bill@solutionreach.com>
2017-11-11 01:12:19 -0700

    Implement BidirectionalKafka  #time 25m

--- src/main/scala/com/billding/kafka/BidirectionalKafka.scala
+++ src/main/scala/com/billding/kafka/BidirectionalKafka.scala
@@ -1,5 +1,25 @@
 package com.billding.kafka
 
-class BidirectionalKafka {
+import java.util
 
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+class BidirectionalKafka(
+  input: String,
+  output: String
+) {
+  val kafkaConfig: KafkaConfig = new KafkaConfig()
+  val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](kafkaConfig.props)
+
+  consumer.subscribe(util.Collections.singletonList(input))
+
+  val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](kafkaConfig.props)
+
+  def send(key: String , value: String ) = {
+    producer.send(
+      new ProducerRecord(
+        output, key, value)
+    )
+  }
 }
