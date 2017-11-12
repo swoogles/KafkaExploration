5170db63920ae802c6ad909b0b86570807bcf010
bfrasure <bill@solutionreach.com>
2017-11-12 16:41:05 -0700

    mk KafkaConfig object. +Clock field  #time 15m

--- src/main/scala/com/billding/kafka/KafkaConfig.scala
+++ src/main/scala/com/billding/kafka/KafkaConfig.scala
@@ -1,5 +1,6 @@
 package com.billding.kafka
 
+import java.time.Clock
 import java.util.Properties
 
 /*
@@ -7,6 +8,26 @@ import java.util.Properties
   topics/servers are actually available.
  */
 class KafkaConfig {
+  val  props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("group.id", "something")
+
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  val TOPIC_1 = "test2"
+  val RAW_WEATHER = "raw_weather"
+
+  val NULL_TOPIC = "This really just means that I need to come up with a less Stringly-typed solution. Until then- weak."
+  val BUSINESS_TOPIC = "business_topic"
+  val PLEASURE_TOPIC = "pleasure_topic"
+  val clock = Clock.systemUTC()
+}
+
+object KafkaConfigPermanent {
   val  props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
 
