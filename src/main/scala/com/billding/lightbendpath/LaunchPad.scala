67ec0eb09407030a23ae62f8b0438188d9228004
bfrasure <bill@solutionreach.com>
2017-11-12 13:51:50 -0700

    Playing with Actors in the launchpad  #time 20m

--- src/main/scala/com/billding/lightbendpath/LaunchPad.scala
+++ src/main/scala/com/billding/lightbendpath/LaunchPad.scala
@@ -2,12 +2,14 @@ package com.billding.lightbendpath
 
 import java.time.Clock
 
-import akka.actor.{ActorSystem, Props}
-import com.billding.akka.RawWeatherActor
+import akka.actor.{ActorSystem, PoisonPill, Props}
+import com.billding.akka.Reaper.WatchMe
+import com.billding.akka.{FunActor, ProductionReaper, RawWeatherActor, Reaper}
 import com.billding.timing.TimedFunctions
 import com.billding.zookeeper.ZookeeperConfig
 import kafka.utils.ZkUtils
 
+import scala.collection.immutable.Range
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
@@ -24,26 +26,57 @@ object LaunchPad extends  App{
   val timedFunctions = new TimedFunctions(clock)
 
   override def main(args: Array[String]): Unit = {
-    val zkUtils: ZkUtils = new ZookeeperConfig().zkUtils
 
+    /*
+    val zkUtils: ZkUtils = new ZookeeperConfig().zkUtils
     val topics: Seq[String] = zkUtils.getAllTopics()
     topics foreach println
+    */
 
     val system = ActorSystem("HelloSystem")
+
+    val reaper = system.actorOf(Props[ProductionReaper], name = "reaper")
+
     val helloActor = system.actorOf(Props[RawWeatherActor], name = "helloactor")
+    reaper ! WatchMe(helloActor)
+
+    val funActor = system.actorOf(Props[FunActor], name = "funActor")
+
+    reaper ! WatchMe(funActor)
+
     system.scheduler.scheduleOnce(
-      50 milliseconds,
+      5 milliseconds,
+      funActor,
+      FunActor.PING
+    )
+
+    system.scheduler.scheduleOnce(
+      5 milliseconds,
+      funActor,
+      "TOTAL GARBAGE"
+    )
+
+
+    system.scheduler.scheduleOnce(
+      5 milliseconds,
       helloActor,
       RawWeatherActor.START_PRODUCING_WEATHER
     )
 
-//    val weatherAlerter = new WeatherAlerter()
-//    weatherAlerter.run()
 
-    val funAlerter = new FunAlerter(timedFunctions)
-    funAlerter.run()
+    system.scheduler.scheduleOnce(
+      50 milliseconds,
+      helloActor,
+      PoisonPill
+    )
 
-    system.terminate()
+
+    system.scheduler.scheduleOnce(
+      50 milliseconds,
+      funActor,
+      PoisonPill
+    )
+
   }
 }
 
