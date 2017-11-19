7745b3d79a7d9257a56e71a167100609a0124bad
bfrasure <bill@solutionreach.com>
2017-11-19 12:41:33 -0700

    FunActor just responds to msgs now, no polling  #time 25m

--- src/main/scala/com/billding/akka/FunActor.scala
+++ src/main/scala/com/billding/akka/FunActor.scala
@@ -1,27 +1,25 @@
 package com.billding.akka
 
 import com.billding.akka.FunActor.PING
-import com.billding.kafka.{KafkaConfigPermanent}
+import com.billding.akka.RawWeatherAlerter.SNOW_ALERT
+import com.billding.kafka.KafkaConfigPermanent
 
 class FunActor
   extends BidirectionalActor(
-    KafkaConfigPermanent.RAW_WEATHER,
+    KafkaConfigPermanent.NULL_TOPIC,
     KafkaConfigPermanent.PLEASURE_TOPIC
   ) {
   val name = "Fun Actor"
 
   def specificReceive: PartialFunction[Any, Unit] = {
-    case PING =>
-      pollWith( record =>{
-        println("Actually got RAW_WEATHER records")
-        if (record.value.contains("Snow")) {
-          bidirectionalKafka.send(
-            "key",
-            s"Snow is coming! Get ready to shred!"
-          )
-        }
-      }
+    case SNOW_ALERT(msg) => {
+      println("got a snow alert in fun actor")
+
+      bidirectionalKafka.send(
+        "key",
+        s"$msg"
       )
+    }
   }
 
 }
